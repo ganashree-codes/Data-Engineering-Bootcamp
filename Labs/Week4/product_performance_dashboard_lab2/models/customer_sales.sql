6e2bba16ad7af18684ac19e1b46766cbc53a7701
ganashree-codes <ganaakash16@gmail.com>
2026-07-28 14:51:34 +0200

    Delete Labs/Week4/product_performance_dashboard_lab2 directory

--- Labs/Week4/product_performance_dashboard_lab2/models/customer_sales.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    product,
-    SUM(amount) AS product_revenue,
-    COUNT(order_id) AS total_orders
-FROM {{ ref('sales_data') }}
-GROUP BY product
-ORDER BY product_revenue DESC
--- Labs/Week4/product_performance_dashboard_lab2/target/compiled/product_performance_dashboard_lab2/models/customer_sales.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    product,
-    SUM(amount) AS product_revenue,
-    COUNT(order_id) AS total_orders
-FROM "dev"."main"."sales_data"
-GROUP BY product
-ORDER BY product_revenue DESC
