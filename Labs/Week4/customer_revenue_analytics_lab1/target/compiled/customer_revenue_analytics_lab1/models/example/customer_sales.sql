f5dec1b549a67a85ec0b53b0cbc4f730f0294ef2
ganashree-codes <ganaakash16@gmail.com>
2026-07-28 14:51:16 +0200

    Delete Labs/Week4/customer_revenue_analytics_lab1 directory

--- Labs/Week4/customer_revenue_analytics_lab1/target/compiled/customer_revenue_analytics_lab1/models/example/customer_sales.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-     customer,
-     COUNT(order_id) AS total_orders,
-     SUM(amount) AS total_sales,
-     AVG(amount) AS average_order_value,
-     SUM(amount) AS revenue
-FROM "dev"."main"."sales_data"
-GROUP BY customer
-ORDER BY revenue DESC
